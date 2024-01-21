3cad6f335b14eb612089982db19885151a634358
superiorsd10 <sachindapkara6@gmail.com>
2024-01-22 02:20:56 +0530

    feat(app): add auth module, created firebase_token_required decorator

---
+++ server/app/auth/firebase_auth.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from flask import request, jsonify, has_request_context
+from firebase_admin import auth
+
+
+def firebase_token_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        request_obj = kwargs.get("request", None)
+
+        if request_obj is None:
+            # If request object is not provided, use Flask's request
+            if has_request_context():
+                request_obj = request
+            else:
+                raise RuntimeError("No Flask request context available.")
+
+        bypass_firebase = (
+            request_obj.headers.get("Bypass-Firebase", "").lower() == "true"
+        )
+
+        if bypass_firebase:
+            return func(*args, **kwargs)
+
+        id_token = request_obj.headers.get("Authorization")
+
+        if not id_token:
+            return jsonify({"error": "Authorization token is missing"}), 401
+
+        try:
+            decoded_token = auth.verify_id_token(id_token)
+            request_obj.current_user = decoded_token
+        except auth.InvalidIdTokenError:
+            return jsonify({"error": "Invalid authorization token"}), 401
+
+        return func(*args, **kwargs)
+
+    return decorated_function
