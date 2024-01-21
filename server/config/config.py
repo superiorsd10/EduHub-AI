96f03724526eccb2becc075bcea8e7be39847c81
superiorsd10 <sachindapkara6@gmail.com>
2024-01-22 02:16:59 +0530

    style(config): formatting the file

--- server/config/config.py
+++ server/config/config.py
@@ -1,24 +1,26 @@
 import os
 
+
 class Config:
     DEBUG = True
     TESTING = True
     MONGODB_SETTINGS = {
         "db": os.environ.get("MONGO_DB"),
-        "host": os.environ.get("MONGO_URI"), 
+        "host": os.environ.get("MONGO_URI"),
         "username": os.environ.get("MONGO_USERNAME"),
         "password": os.environ.get("MONGO_PASSWORD"),
         "alias": "default",
     }
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
     FIREBASE_CREDENTIALS = os.environ.get("FIREBASE_CREDENTIALS")
-    
+
+
 class TestConfig:
     DEBUG = True
     TESTING = True
     MONGODB_SETTINGS = {
         "db": os.environ.get("MONGO_TEST_DB"),
-        "host": os.environ.get("MONGO_TEST_URI"), 
+        "host": os.environ.get("MONGO_TEST_URI"),
         "username": os.environ.get("MONGO_USERNAME"),
         "password": os.environ.get("MONGO_PASSWORD"),
         "alias": "default",
