48b3af3888625d5a75230ff2690f13a3aba98bad
superiorsd10 <sachindapkara6@gmail.com>
2024-01-22 02:16:16 +0530

    style(app): formatting the file

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,28 +5,25 @@ from config.config import Config
 from firebase_admin import credentials, initialize_app
 from dotenv import load_dotenv
 
+
 def create_app(config=None):
-    
     app = Flask(__name__)
-    
+
     load_dotenv()
-    
+
     if config:
         app.config.from_object(config)
-    
+
     mongo_config = app.config.get("MONGODB_SETTINGS")
-    
-    # Assuming your test script is in the 'tests/auth' directory
+
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # Navigate up to the 'server' directory and then go to 'credentials'
     key_file_path = os.path.abspath(
         os.path.join(current_dir, "../credentials/service_account_key.json")
     )
 
     cred = credentials.Certificate(key_file_path)
 
-    # Initialize Firebase in the testing context
     firebase_app = initialize_app(cred)
 
     try:
@@ -39,11 +36,11 @@ def create_app(config=None):
         print("Connected to MongoDB successfully!")
     except Exception as e:
         print(f"Failed to connect to MongoDB: {e}")
-        
+
     return app
 
+
 def create_test_app():
-    
     app = Flask(__name__)
-    
+
     return app
