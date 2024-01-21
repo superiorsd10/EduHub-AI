464dcf6266ec1817caccb6ceb103ad5a98736611
superiorsd10 <sachindapkara6@gmail.com>
2024-01-22 02:18:10 +0530

    feat(run): register user_routes blueprint

--- server/run.py
+++ server/run.py
@@ -1,23 +1,15 @@
 from app import create_app
-from flask import Flask
-from mongoengine import connect
 from flask_jwt_extended import JWTManager
 from firebase_admin import *
-from firebase_admin import credentials
 from config.config import Config
 
-app = create_app()
-
-app.config.from_object(Config)
-
-connect(
-    db=app.config["MONGODB_SETTINGS"]["db"],
-    host=app.config["MONGODB_SETTINGS"]["host"],
-    username=app.config["MONGODB_SETTINGS"]["username"],
-    password=app.config["MONGODB_SETTINGS"]["password"],
-)
+app = create_app(Config)
 
 jwt = JWTManager(app)
+
+from app.routes.user_routes import user_blueprint
+
+app.register_blueprint(user_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
