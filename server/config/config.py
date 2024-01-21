fd82942d88bb331cc5bd04570125972b2b928551
superiorsd10 <sachindapkara6@gmail.com>
2024-01-22 02:14:57 +0530

    feat(config): add TestConfig

--- server/config/config.py
+++ server/config/config.py
@@ -12,3 +12,16 @@ class Config:
     }
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
     FIREBASE_CREDENTIALS = os.environ.get("FIREBASE_CREDENTIALS")
+    
+class TestConfig:
+    DEBUG = True
+    TESTING = True
+    MONGODB_SETTINGS = {
+        "db": os.environ.get("MONGO_TEST_DB"),
+        "host": os.environ.get("MONGO_TEST_URI"), 
+        "username": os.environ.get("MONGO_USERNAME"),
+        "password": os.environ.get("MONGO_PASSWORD"),
+        "alias": "default",
+    }
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    FIREBASE_CREDENTIALS = os.environ.get("FIREBASE_CREDENTIALS")
