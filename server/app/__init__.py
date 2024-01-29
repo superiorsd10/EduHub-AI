99898cbcc5cdf142e4e26d3def6dff507d24c3a2
superiorsd10 <sachindapkara6@gmail.com>
2024-01-29 09:38:15 +0530

    chore(app): add flask-redis, Flask-Limiter to requirements, initialize redis, limiter in create_app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -8,6 +8,9 @@ from mongoengine import connect
 from config.config import Config
 from firebase_admin import credentials, initialize_app
 from dotenv import load_dotenv
+from flask_redis import FlaskRedis
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 
 def create_app(config=None):
@@ -23,6 +26,8 @@ def create_app(config=None):
 
     if config:
         app.config.from_object(config)
+    else:
+        app.config.from_object(Config)
 
     mongo_config = app.config.get("MONGODB_SETTINGS")
 
@@ -37,6 +42,19 @@ def create_app(config=None):
     firebase_app = initialize_app(cred)
 
     print(firebase_app.name)
+
+    redis = FlaskRedis(app)
+
+    print(redis.config_prefix)
+
+    limiter = Limiter(
+        key_func=get_remote_address,
+        app=app,
+        default_limits=["1000 per day", "100 per hour"],
+        storage_uri=app.config["REDIS_URL"],
+    )
+
+    print(limiter.current_limit)
 
     try:
         connect(
