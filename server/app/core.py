3296eba8b90f3fe4ed933ceff0e19311e26db16f
superiorsd10 <sachindapkara6@gmail.com>
2024-02-08 19:17:44 +0530

    chore(config): add redis_client to Config

--- server/app/core.py
+++ server/app/core.py
@@ -4,7 +4,6 @@ Module: core
 This module initializes the FlaskRedis and Limiter instances used in the application.
 
 Attributes:
-    - redis: An instance of FlaskRedis for interacting with Redis.
     - limiter: An instance of Limiter for rate limiting requests.
 
 Usage:
@@ -13,15 +12,9 @@ Usage:
 """
 
 import os
-import redis
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-redis_client = redis.Redis(
-    host=os.environ.get("REDIS_HOST"),
-    port=os.environ.get("REDIS_PORT"),
-    password=os.environ.get("REDIS_PASSWORD"),
-)
 
 limiter = Limiter(
     key_func=get_remote_address,
