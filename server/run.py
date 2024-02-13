bbf254ce8b5d0614670e7639dcaa68039ba68d27
superiorsd10 <sachindapkara6@gmail.com>
2024-02-13 18:35:57 +0530

    chore(run): register hub_blueprint

--- server/run.py
+++ server/run.py
@@ -6,6 +6,7 @@ from app.app import create_app
 from config.config import Config
 from flask_jwt_extended import JWTManager
 from app.routes.user_routes import user_blueprint
+from app.routes.hub_routes import hub_blueprint
 
 
 app = create_app(Config)
@@ -14,6 +15,7 @@ jwt = JWTManager(app)
 
 
 app.register_blueprint(user_blueprint)
+app.register_blueprint(hub_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
