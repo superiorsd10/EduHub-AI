825d11107c0ad2028e1fd67ed8258d7d2e869988
superiorsd10 <sachindapkara6@gmail.com>
2024-02-19 15:22:10 +0530

    test(routes): update the user_routes test

--- server/tests/routes/test_user_routes.py
+++ server/tests/routes/test_user_routes.py
@@ -1,153 +1,55 @@
 """
-Module for testing user routes in the app.
+Tests for user routes in the Flask application.
 """
 
-import os
-from unittest.mock import patch
-import pytest
+import json
+import unittest
+from unittest.mock import patch, MagicMock
 from app.enums import StatusCode
 from app.routes.user_routes import user_blueprint
-from app.models.user import User
 from app.app import create_test_app
-from config.config import TestConfig
-from dotenv import load_dotenv
-from mongoengine import connect, disconnect
-
-load_dotenv()
-
-app = create_test_app()
-app.config.from_object(TestConfig)
-app.config["TESTING"] = True
-app.config["DEBUG"] = False
 
 
-app.config["MONGODB_SETTINGS"] = {
-    # type: ignore
-    "db": os.environ.get("MONGO_TEST_DB"),
-    "host": os.environ.get("MONGO_TEST_URI"),
-    "username": os.environ.get("MONGO_USERNAME"),
-    "password": os.environ.get("MONGO_PASSWORD"),
-    "alias": "default",
-}
+class TestUserRoutes(unittest.TestCase):
+    """
+    Test class for user routes functionality.
+    """
 
-mongo_config = app.config.get("MONGODB_SETTINGS")
+    def setUp(self):
+        self.app = create_test_app()
+        self.app.config["TESTING"] = True
+        self.app.register_blueprint(user_blueprint)
+        self.client = self.app.test_client()
 
-app.register_blueprint(user_blueprint)
+    @patch("app.routes.user_routes.User")
+    @patch("app.routes.user_routes.Config.redis_client", MagicMock())
+    @patch("app.routes.user_routes.firebase_token_required", MagicMock())
+    async def test_create_user_success(self, mock_user):
+        """
+        Tests successful user creation with valid data and bypasses Firebase auth.
+        """
 
+        with self.app.test_request_context():
+            with patch("app.routes.user_routes.request") as mock_request:
+                headers = {
+                    "Bypass-Firebase": "true",
+                }
+                mock_request.headers = headers
+                mock_request.get_json.return_value = {
+                    "name": "Test User",
+                    "email": "test@example.com",
+                }
+                mock_user_instance = mock_user.return_value
+                mock_user_instance.id = "user_id"
 
-@pytest.fixture
-def client():
-    """Fixture for creating a test client."""
-    return app.test_client()
+                response = await self.client.post("/api/sign-up", headers=headers)
 
+                data = json.loads(response.data.decode("utf-8"))
 
-@pytest.fixture(scope="session", autouse=True)
-def setup_teardown(request):
-    """Fixture for setting up and tearing down the test environment."""
-    disconnect(alias="default")
-
-    try:
-        connect(
-            db=mongo_config["db"],
-            host=mongo_config["host"],
-            username=mongo_config["username"],
-            password=mongo_config["password"],
-            alias=mongo_config["alias"],
-        )
-        print("Connected to MongoDB successfully!")
-    except Exception as exception:
-        print(f"Failed to connect to MongoDB: {exception}")
-
-    # Teardown: Drop the test database after the test
-    def teardown():
-        """Teardown function to disconnect from MongoDB."""
-        disconnect(alias="default")
-
-    request.addfinalizer(teardown)
-
-
-@pytest.mark.parametrize(
-    "user_id, payload, expected_status, expected_response",
-    [
-        (
-            "user_id_1",
-            {"name": "John Doe", "email": "john7@example.com"},
-            StatusCode.CREATED.value,
-            {"message": "User created successfully", "success": True},
-        ),
-    ],
-)
-def test_create_user(
-    client, mocker, user_id, payload, expected_status, expected_response
-):
-    """Test creating a user with valid data."""
-    with app.test_request_context():
-        response = client.post(
-            "/api/sign-up",
-            json=payload,
-            headers={"user_id": user_id, "Bypass-Firebase": "true"},
-        )
-
-        print(response.get_json())
-
-        assert response.status_code == expected_status
-        assert response.json == expected_response
-
-        User.objects(email=payload["email"]).delete()
-        disconnect(alias="default")
-
-
-@pytest.mark.parametrize(
-    "user_id, payload, expected_status, expected_response",
-    [
-        (
-            "user_id_1",
-            {"email": "john@example.com"},
-            StatusCode.BAD_REQUEST.value,
-            {"error": "Invalid data provided", "success": False},
-        ),
-    ],
-)
-def test_create_user_invalid_data(
-    client, mocker, user_id, payload, expected_status, expected_response
-):
-    """Test creating a user with invalid data."""
-
-    with app.test_request_context(
-        headers={"Bypass-Firebase": "true"},
-        json=payload,
-    ):
-        response = client.post(
-            "/api/sign-up",
-            json=payload,
-            headers={"Bypass-Firebase": "true"},
-        )
-
-    assert response.status_code == expected_status
-    assert response.get_json() == expected_response
-
-
-def test_create_user_exception(client):
-    """Test creating a user with an exception in the save method."""
-    user_data = {"name": "John Doe", "email": "john@example.com"}
-
-    with app.test_request_context(
-        headers={"Bypass-Firebase": "true"},
-        json=user_data,
-    ), patch("app.routes.user_routes.User.save") as mock_save:
-        mock_save.side_effect = Exception("Mocked exception")
-
-        response = client.post(
-            "/api/sign-up",
-            json=user_data,
-            headers={"Bypass-Firebase": "true"},
-        )
-
-    print(response.get_json())
-
-    assert response.status_code == StatusCode.INTERNAL_SERVER_ERROR.value
-    assert response.get_json() == {"error": "Mocked exception", "success": False}
+                self.assertEqual(response.status_code, StatusCode.CREATED.value)
+                self.assertTrue(data["success"])
+                self.assertEqual(data["message"], "User created successfully")
 
 
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
