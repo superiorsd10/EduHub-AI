8ebed13f1e897fb54a4859f5dbac77fa41d3e342
superiorsd10 <sachindapkara6@gmail.com>
2024-03-03 15:48:55 +0530

    chore(server): register process uploaded file task in run.py

--- server/run.py
+++ server/run.py
@@ -9,6 +9,7 @@ from app.routes.user_routes import user_blueprint
 from app.routes.hub_routes import hub_blueprint
 from app.routes.post_routes import post_blueprint
 from app.sockets.hub_sockets import handle_accept_request, handle_reject_request
+from app.celery.tasks.post_tasks import process_uploaded_file
 
 
 app, celery_instance = create_app(Config)
@@ -22,6 +23,8 @@ app.register_blueprint(post_blueprint)
 
 socketio.on_event("accept-request", handle_accept_request)
 socketio.on_event("reject_request", handle_reject_request)
+
+celery_instance.register_task(process_uploaded_file)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
