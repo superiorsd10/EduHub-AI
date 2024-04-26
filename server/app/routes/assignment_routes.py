91388dddbded0f168b419de7ecde9de133f12eeb
superiorsd10 <sachindapkara6@gmail.com>
2024-04-26 07:14:58 +0530

    chore(routes): add the generate assignment endpoint to assignment routes

--- server/app/routes/assignment_routes.py
+++ server/app/routes/assignment_routes.py
@@ -4,7 +4,12 @@ Assignment routes for the Flask application.
 
 import base64
 from bson import ObjectId
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.auth.firebase_auth import firebase_token_required
+from app.enums import StatusCode
+from app.core import limiter
+from app.models.hub import Hub
+from app.celery.tasks.assignment_tasks import process_assignment_generation
 from marshmallow import Schema, fields
 
 assignment_blueprint = Blueprint("assignment", __name__)
@@ -75,3 +80,85 @@ def decode_base64_to_objectid(base64_encoded: str) -> ObjectId:
     hex_string = decoded_bytes.decode("utf-8")
     object_id = ObjectId(hex_string)
     return object_id
+
+
+@assignment_blueprint.route("/api/<hub_id>/generate-assignment", methods=["POST"])
+@limiter.limit("5 per minute")
+@firebase_token_required
+def generate_assignment(hub_id):
+    """
+    Generate an assignment for a given hub.
+
+    This endpoint generates an assignment for the specified hub
+    based on the provided parameters.
+    It first validates the input data using the GenerateAssignmentSchema
+    schema. Then, it retrieves
+    the hub data from the database using the provided hub ID and checks
+    if the hub exists. If the hub
+    exists, it determines the number of existing assignments in the hub
+    to calculate the count of the
+    new assignment. Finally, it asynchronously processes the assignment
+    generation task using Celery.
+
+    Args:
+        hub_id (str): The ID of the hub for which the assignment is generated.
+
+    Returns:
+        tuple: A tuple containing the JSON response and HTTP status code.
+
+    Raises:
+        Exception: If an error occurs during assignment generation or validation.
+
+    """
+    try:
+        schema = GenerateAssignmentSchema()
+        data = schema.load(request.get_json())
+
+        title = data.get("title")
+        topics = data.get("topics")
+        specific_topics = data.get("specific_topics")
+        instructions_for_ai = data.get("instructions_for_ai")
+        types_of_questions = data.get("types_of_questions")
+
+        hub_object_id = decode_base64_to_objectid(base64_encoded=hub_id)
+        print(hub_object_id)
+        hub_data = Hub.objects(id=hub_object_id).first()
+        print(hub_data)
+
+        if not hub_data:
+            return (
+                jsonify({"error": "Hub not found", "success": False}),
+                StatusCode.NOT_FOUND.value,
+            )
+
+        assignments_count = len(hub_data.assignments)
+        print(assignments_count)
+
+        process_assignment_generation.apply_async(
+            args=[
+                title,
+                topics,
+                specific_topics,
+                instructions_for_ai,
+                types_of_questions,
+                hub_id,
+                assignments_count,
+            ],
+            retry_policy={
+                "max_retries": 3,
+                "interval_start": 2,
+                "interval_step": 2,
+                "interval_max": 10,
+            },
+        )
+
+        return (
+            jsonify({"message": "Generating the assignment", "success": True}),
+            StatusCode.SUCCESS.value,
+        )
+
+    except Exception as error:
+        return (
+            jsonify({"error": str(error), "success": False}),
+            StatusCode.INTERNAL_SERVER_ERROR.value,
+        )
