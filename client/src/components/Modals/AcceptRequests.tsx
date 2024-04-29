d4526114509e10159407179f553b60c682ad0329
then00bprogrammer <lcs2021006@iiitl.ac.in>
2024-04-29 17:57:03 +0530

    feat(join hub): added functionality to accept/reject join hub requests

--- client/src/components/Modals/AcceptRequests.tsx
+++ client/src/components/Modals/AcceptRequests.tsx
@@ -1,26 +1,33 @@
 import { Avatar, Button, Group, Modal, Stack, Text } from "@mantine/core";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { HubContext } from "@/providers/HubProvider";
 import NextLink from "@/utils/NextLink";
 import { FaCircleCheck } from "react-icons/fa6";
 import { RxCross1 } from "react-icons/rx";
 import { useRouter } from "next/router";
+import { AppContext } from "@/providers/AppProvider";
 
-const AcceptRequests:React.FC<{id: string;}> = ({id}) => {
+const AcceptRequests: React.FC<{ id: string }> = ({ id }) => {
   const { setIsAcceptRequestsVisible, isAcceptRequestsVisible } =
     useContext(HubContext);
+  const { socket } = useContext(AppContext)
   const router = useRouter();
-  const hub_id=router.query.hub_id as string;
+  const hub_id = router.query.hub_id as string;
 
-  const getInvitationList = async()=>{
-    const resp=await fetch(`http://127.0.0.1:5000/api/${hub_id}/get-invitation-list`);
-    const body=await resp.json();
-    console.log(body);
-  }
+  const [invitationList, setInvitationList] = useState<string[]>([]);
 
-  useEffect(()=>{
+  const getInvitationList = async () => {
+    const resp = await fetch(
+      `http://127.0.0.1:5000/api/${btoa(hub_id)}/get-invitation-list`
+    );
+    const body = await resp.json();
+    const list: string[] = body.message;
+    setInvitationList(list);
+  };
+
+  useEffect(() => {
     getInvitationList();
-  },[])
+  }, []);
   return (
     <Modal
       opened={isAcceptRequestsVisible}
@@ -30,51 +37,33 @@ const AcceptRequests:React.FC<{id: string;}> = ({id}) => {
       radius="md"
       zIndex={10001}
     >
-      <Stack mt='sm'>
-        <Group justify="space-between" pl='lg' pr='lg'>
-          <Group>
-            <Avatar/>
-            <Text>Nikhil Ranjan</Text>
+      <Stack mt="sm">
+        {invitationList.map((email) => (
+          <Group justify="space-between" pl="lg" pr="lg">
+            <Group>
+              <Avatar />
+              <Text>Nikhil Ranjan</Text>
+            </Group>
+            <Group>
+              <FaCircleCheck size="24px" onClick={()=>{
+                  socket.emit("accept-request", {
+                    email: email,
+                    hub_id: hub_id
+                  });
+              }}/>
+              <RxCross1 size="24px" onClick={()=>{
+                  socket.emit("reject-request", {
+                    email: email,
+                    hub_id: hub_id
+                  });
+              }}/>
+            </Group>
           </Group>
-          <Group>
-            <FaCircleCheck size="24px"/>
-            <RxCross1 size="24px"/>
-          </Group>
-        </Group>
-        <Group justify="space-between" pl='lg' pr='lg'>
-          <Group>
-            <Avatar/>
-            <Text>Nikhil Ranjan</Text>
-          </Group>
-          <Group>
-            <FaCircleCheck size="24px"/>
-            <RxCross1 size="24px"/>
-          </Group>
-        </Group>
-        <Group justify="space-between" pl='lg' pr='lg'>
-          <Group>
-            <Avatar/>
-            <Text>Nikhil Ranjan</Text>
-          </Group>
-          <Group>
-            <FaCircleCheck size="24px"/>
-            <RxCross1 size="24px"/>
-          </Group>
-        </Group>
-        <Group justify="space-between" pl='lg' pr='lg'>
-          <Group>
-            <Avatar/>
-            <Text>Nikhil Ranjan</Text>
-          </Group>
-          <Group>
-            <FaCircleCheck size="24px"/>
-            <RxCross1 size="24px"/>
-          </Group>
-        </Group>
+        ))}
       </Stack>
       <Group mt="lg" justify="center">
         <Button
-          onClick={()=>setIsAcceptRequestsVisible(false)}
+          onClick={() => setIsAcceptRequestsVisible(false)}
           variant="default"
           radius="md"
           color="black"
