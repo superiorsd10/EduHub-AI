cf60373532b568a94c9ed9fd641264abb579bf24
then00bprogrammer <lcs2021006@iiitl.ac.in>
2024-05-02 08:42:36 +0530

    fix(generate assignment): changed api route

--- client/src/pages/api/subscribe.ts
+++ client/src/pages/api/subscribe.ts
@@ -3,34 +3,44 @@ import Redis from 'ioredis';
 export default async function handler(req:any, res:any) {
   if (req.method === 'GET') {
     const { id } = req.query;
-    const key=`generate_assignment_id_${id}`
-    
+    const key = `generate_assignment_id_${id}`;
+
     const redis = new Redis(process.env.NEXT_PUBLIC_REDIS_URL as string);
 
-    redis.subscribe(key, (err, count) => {
-      if (err) {
-        console.error('Error subscribing to channel', err);
-        return res.status(500).json({ error: 'Error subscribing to channel' });
-      }
-      console.log(`Subscribed to ${count} channel.`);
-    });
+    const subscriptionPromise = new Promise((resolve, reject) => {
+      redis.subscribe(key, (err, count) => {
+        if (err) {
+          console.error('Error subscribing to channel', err);
+          reject(err);
+        }
+        console.log(`Subscribed to ${count} channel.`);
+      });
 
-    redis.on('message', (channel, message) => {
-      console.log(`Received message ${message} from channel ${channel}`);
-      
-      const parsedMessage = JSON.parse(message);
-      if (parsedMessage.id === id) {
-        console.log('Received message with desired ID:', parsedMessage);
-        res.status(200).json(parsedMessage); // Send response here
+      redis.on('message', (channel, message) => {
+        console.log(`Received message ${message} from channel ${channel}`);
+        
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.id === id) {
+          console.log('Received message with desired ID:', parsedMessage);
+          resolve(parsedMessage);
+          redis.unsubscribe();
+          redis.quit();
+        }
+      });
+
+      req.on('close', () => {
         redis.unsubscribe();
         redis.quit();
-      }
+        reject(new Error('Request closed'));
+      });
     });
 
-    req.on('close', () => {
-      redis.unsubscribe();
-      redis.quit();
-    });
+    try {
+      const data = await subscriptionPromise;
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: "Assignment not generated" });
+    }
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
